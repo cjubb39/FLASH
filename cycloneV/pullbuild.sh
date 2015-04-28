10cabc1ad151d6787d2c9ece365c2b3f997fe30c
kugurst <ma2799@gmail.com>
2015-04-28 00:31:03 -0400

    Pushed the relevant submodules
    You need linux-socfpga and u-boot-socfpga

--- cycloneV/pullbuild.sh
+++ cycloneV/pullbuild.sh
@@ -6,15 +6,25 @@ fi
 if [[ ! -e gcc-linaro-arm-linux-gnueabihf-4.7-2012.11-20121123_linux ]]; then
     tar xjf gcc-linaro-arm-linux-gnueabihf-4.7-2012.11-20121123_linux.tar.bz2
 fi
-if [[ ! -e soc_system_13_0_0_06252013_90253.tar.gz ]]; then
-    wget http://www.rocketboards.org/pub/Projects/SoCKitLinaroLinuxDesktop/soc_system_13_0_0_06252013_90253.tar.gz
-fi
-
-SOCDIR="soc_system_13_0_0_06252013_90253"
-if [[ ! -e "$SOCDIR" ]]; then
-    mkdir "$SOCDIR"
-    cd "$SOCDIR"
-    tar xf ../soc_system_13_0_0_06252013_90253.tar.gz
-    cd ../
-fi
+# if [[ ! -e soc_system_13_0_0_06252013_90253.tar.gz ]]; then
+#     wget http://www.rocketboards.org/pub/Projects/SoCKitLinaroLinuxDesktop/soc_system_13_0_0_06252013_90253.tar.gz
+# fi
+# 
+# SOCDIR="soc_system_13_0_0_06252013_90253"
+# if [[ ! -e "$SOCDIR" ]]; then
+#     mkdir "$SOCDIR"
+#     cd "$SOCDIR"
+#     tar xf ../soc_system_13_0_0_06252013_90253.tar.gz
+#     cd ../
+# fi
+# if [[ ! -e linaro-quantal-nano-20130422-342.tar.gz ]]; then
+#     wget https://releases.linaro.org/13.04/ubuntu/quantal-images/nano/linaro-quantal-nano-20130422-342.tar.gz
+# fi
+# LINARO="linaro-quantal-nano-20130422-342"
+# if [[ ! -e "$LINARO" ]]; then
+#     mkdir "$LINARO"
+#     cd "$LINARO"
+#     tar xf ../linaro-quantal-nano-20130422-342.tar.gz
+#     cd ../
+# fi
 
