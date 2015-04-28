36e9e083801c32fe1c3e49b2e077203224fe21d1
kugurst <ma2799@gmail.com>
2015-04-28 00:56:46 -0400

    Added all the junk in my directory to .gitignore.

--- cycloneV/pullbuild.sh
+++ cycloneV/pullbuild.sh
@@ -6,6 +6,15 @@ fi
 if [[ ! -e gcc-linaro-arm-linux-gnueabihf-4.7-2012.11-20121123_linux ]]; then
     tar xjf gcc-linaro-arm-linux-gnueabihf-4.7-2012.11-20121123_linux.tar.bz2
 fi
+if [[ ! -e buildroot-2013.11.tar.gz ]]; then
+    wget http://buildroot.uclibc.org/downloads/buildroot-2013.11.tar.gz
+fi
+if [[ ! -e buildroot-2013.11 ]]; then
+    tar xf buildroot-2013.11.tar.gz
+fi
+if [[ ! -e bootloader.img ]]; then
+    wget https://dl.dropboxusercontent.com/u/22450509/fpga-series/bootloader.img
+fi
 # if [[ ! -e soc_system_13_0_0_06252013_90253.tar.gz ]]; then
 #     wget http://www.rocketboards.org/pub/Projects/SoCKitLinaroLinuxDesktop/soc_system_13_0_0_06252013_90253.tar.gz
 # fi
